83d10350509467aac73d24f335f4313a05768c33
bagrounds <bagrounds@gmail.com>
2018-07-24 17:51:20 -0700

    [PATCH][ADD] more thorough tests

--- test/tests.js
+++ test/tests.js
@@ -2,28 +2,29 @@
   'use strict'
 
   /* imports */
-  const { map } = require('fun-array')
+  const { map, append } = require('fun-array')
   const { sync } = require('fun-test')
   const { equalDeep } = require('fun-predicate')
   const { get } = require('fun-object')
+  const arrange = require('fun-arrange')
+  const { pipeAll } = require('fun-function')
 
-  const equalityTests = [
-    {
-      inputs: [['a'], { a: 1 }],
-      predicate: equalDeep(1),
-      contra: get('get')
-    },
-    {
-      inputs: [[1], [8, 3]],
-      predicate: equalDeep(3),
-      contra: get('get')
-    },
-    {
-      inputs: [[[2]], n => n * 2],
-      predicate: equalDeep(4),
-      contra: get('get')
-    }
-  ]
+  const equalityTests = map(
+    pipeAll([
+      append(get('get')),
+      arrange({ inputs: 0, predicate: 1, contra: 2 })
+    ]),
+    [
+      [[['a'], { a: 1 }], equalDeep(1)],
+      [[[1], [8, 3]], equalDeep(3)],
+      [[[[2]], n => n * 2], equalDeep(4)],
+      [[['a'], { a: [1] }], equalDeep([1])],
+      [[['a', 0], { a: [1] }], equalDeep(1)],
+      [[['a', 0, [2]], { a: [x => x * 2] }], equalDeep(4)],
+      [[['a', 0, [2]], { a: [x => [x, x * 2]] }], equalDeep([2, 4])],
+      [[['a', 0, [2], 1], { a: [x => [x, x * 2]] }], equalDeep(4)]
+    ]
+  )
 
   /* exports */
   module.exports = map(sync, equalityTests)
