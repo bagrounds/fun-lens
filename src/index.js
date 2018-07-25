497cb5fd4c7d6690a2b601ee8a92fcd53f7e8777
bagrounds <bagrounds@gmail.com>
2018-07-25 08:32:13 -0700

    [MINOR][ADD] set and update functions

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@
   'use strict'
 
   /* imports */
-  const { curry, apply } = require('fun-function')
+  const funFunction = require('fun-function')
+  const { curry } = funFunction
   const { some } = require('fun-predicate')
-  const { ap, map, get: objectGet } = require('fun-object')
-  const { fun, tuple, object, string, array } = require('fun-type')
-  const { get: arrayGet } = require('fun-array')
+  const funObject = require('fun-object')
+  const { ap, map } = funObject
+  const { any, num, fun, tuple, object, string, array } = require('fun-type')
+  const funArray = require('fun-array')
   const { inputs } = require('guarded')
+  const { first } = require('fun-case')
+
+  const choose = role => first([
+    [array, funFunction[role]],
+    [string, funObject[role]],
+    [num, funArray[role]]
+  ])
 
   /**
    *
@@ -23,19 +32,54 @@
    * @return {*} value at keys
    */
   const get = (keys, source) => keys.reduce(
-    (source, key) => (array(key)
-      ? apply
-      : string(key)
-        ? objectGet
-        : arrayGet
-    )(key)(source),
+    (source, key) => choose('get')(key)(source),
     source
   )
 
+  /**
+   *
+   * @function module:fun-lens.set
+   *
+   * @param {Array} keys - to access underlying values
+   * @param {*} value - to set
+   * @param {Array|Object|Function} source - to access values from
+   *
+   * @return {*} value at keys
+   */
+  const set = (keys, value, source) =>
+    keys.length === 1
+      ? choose('set')(keys[0])(value)(source)
+      : set(
+        keys.slice(0, 1),
+        set(keys.slice(1), value, get(keys.slice(0, 1), source)),
+        source
+      )
+
+  /**
+   *
+   * @function module:fun-lens.update
+   *
+   * @param {Array} keys - to access underlying values
+   * @param {Function} u - to apply at key
+   * @param {Array|Object|Function} source - to update value on
+   *
+   * @return {*} value at keys
+   */
+  const update = (keys, u, source) =>
+    keys.length === 1
+      ? choose('update')(keys[0])(u)(source)
+      : update(
+        keys.slice(0, 1),
+        curry(update)(keys.slice(1), u),
+        source
+      )
+
   /* exports */
-  const api = { get }
+  const api = { get, set, update }
   const guards = map(inputs, {
-    get: tuple([array, some([array, object, fun])])
+    get: tuple([array, some([array, object, fun])]),
+    set: tuple([array, any, some([array, object, fun])]),
+    update: tuple([array, fun, some([array, object, fun])])
   })
 
   module.exports = map(curry, ap(guards, api))
--- test/tests.js
+++ test/tests.js
@@ -2,28 +2,81 @@
   'use strict'
 
   /* imports */
-  const { map, append } = require('fun-array')
+  const { fold, concat, map, append } = require('fun-array')
   const { sync } = require('fun-test')
   const { equalDeep } = require('fun-predicate')
   const { get } = require('fun-object')
   const arrange = require('fun-arrange')
-  const { pipeAll } = require('fun-function')
+  const { compose, apply } = require('fun-function')
+  const { mul, add } = require('fun-scalar')
 
   const equalityTests = map(
-    pipeAll([
-      append(get('get')),
-      arrange({ inputs: 0, predicate: 1, contra: 2 })
-    ]),
-    [
-      [[['a'], { a: 1 }], equalDeep(1)],
-      [[[1], [8, 3]], equalDeep(3)],
-      [[[[2]], n => n * 2], equalDeep(4)],
-      [[['a'], { a: [1] }], equalDeep([1])],
-      [[['a', 0], { a: [1] }], equalDeep(1)],
-      [[['a', 0, [2]], { a: [x => x * 2] }], equalDeep(4)],
-      [[['a', 0, [2]], { a: [x => [x, x * 2]] }], equalDeep([2, 4])],
-      [[['a', 0, [2], 1], { a: [x => [x, x * 2]] }], equalDeep(4)]
-    ]
+    arrange({ inputs: 0, predicate: 1, contra: 2 }),
+    fold(concat, [], [
+      map(append(get('get')), [
+        [[['a'], {}], equalDeep(null)],
+        [[['a'], { a: 1 }], equalDeep(1)],
+        [[[1], [8, 3]], equalDeep(3)],
+        [[[[2]], n => n * 2], equalDeep(4)],
+        [[['a'], { a: [1] }], equalDeep([1])],
+        [[['a', 0], { a: [1] }], equalDeep(1)],
+        [[['a', 0, [2]], { a: [x => x * 2] }], equalDeep(4)],
+        [[['a', 0, [2]], { a: [x => [x, x * 2]] }], equalDeep([2, 4])],
+        [[['a', 0, [2], 1], { a: [x => [x, x * 2]] }], equalDeep(4)]
+      ]),
+      map(append(get('set')), [
+        [[['a'], 6, { a: 1 }], equalDeep({ a: 6 })],
+        [[[1], 9, [8, 3]], equalDeep([8, 9])],
+        [
+          [[[2]], -1, n => n * 2],
+          compose(
+            equalDeep(-1),
+            apply([2])
+          )
+        ],
+        [
+          [[[2]], -1, n => n * 2],
+          compose(
+            equalDeep(6),
+            apply([3])
+          )
+        ],
+        [[['a', 0], 11, { a: [1] }], equalDeep({ a: [11] })],
+        [[[0, 'a'], 11, [{ a: 1 }]], equalDeep([{ a: 11 }])],
+        [
+          [[0, 'a', 1], 11, [{ a: [1, 2] }]],
+          equalDeep([{ a: [1, 11] }])
+        ],
+        [
+          [['a', 0, [2], 0], 42, { a: [x => [x, x * 2]] }],
+          compose(
+            equalDeep([42, 4]),
+            ({ a: [f] }) => f(2)
+          )
+        ],
+        [
+          [['a', 0, [2], 0], 42, { a: [x => [x, x * 2]] }],
+          compose(
+            equalDeep([3, 6]),
+            ({ a: [f] }) => f(3)
+          )
+        ]
+      ]),
+      map(append(get('update')), [
+        [[['a'], mul(3), { a: 2 }], equalDeep({ a: 6 })],
+        [[[1], add(4), [8, 3]], equalDeep([8, 7])],
+        [[[1, 0], add(4), [8, [3]]], equalDeep([8, [7]])],
+        [[['a', 0], add(4), { a: [3, 8] }], equalDeep({ a: [7, 8] })],
+        [[[1, 'a'], add(4), [8, { a: 3 }]], equalDeep([8, { a: 7 }])],
+        [
+          [['a', 0, [2], 0], add(3), { a: [x => [x, x * 2]] }],
+          compose(
+            equalDeep([5, 4]),
+            ({ a: [f] }) => f(2)
+          )
+        ]
+      ])
+    ])
   )
 
   /* exports */
